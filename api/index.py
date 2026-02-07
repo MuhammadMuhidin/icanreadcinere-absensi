95fc756e0d5634e9c264922ef2fe006f7d510177
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-08 03:02:17 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -789,7 +789,7 @@ def check_missed_attendance():
     return {
         "show": True,
         "message":(
-            "\n\nIt seems that you may have forgotten to complete "
+            "It seems that you may have forgotten to complete "
             "your check-in or check-out yesterday."
         )
     }
